ea84c3c50c430734982370423792abfeb4735bc0
BDib <BDib@users.noreply.github.com>
2026-06-22 20:55:57 +0100

    Deploy StackyNG documentation

--- src/resource.h
+++
@@ -1,6 +0,0 @@
-#define STACKY_ICON_ID		101
-
-#define STACKY_VERSION          0,7,0
-#define STACKY_VERSION_STR      L"0.7.0"
-
-#define STACKY_COPYRIGHT_STR    L"2013-2024 Tengi Design, Pawel Turlejski & CTW B D Dib"
